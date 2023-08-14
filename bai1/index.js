96236218f52b118f96054364b8ac7c0941edcf17
nguyenphanlongvu <vuvt6019@gmail.com>
2023-08-14 09:24:52 +0700

    update file bai5

--- bai1/index.js
+++ bai1/index.js
@@ -1,10 +1,7 @@
- function tinhChuso() {
-
-    var chuSo = document.getElementById("chuSo").value*1
-    var hangChuc = math.floor(chuSo / 10);
-    var hangDonvi = chuSo % 10 
-    var tongChuso = hangChuc + hangDonvi
-    var ketQuaElement = document.getElementById("ketQua")
-    ketQuaElement.innerHTML = "Tổng chữ số là : " + tongChuso 
-
- }
+ function Tinhluong() {
+    var soNgaylamviec = document.getElementById("soNgaylamviec").value*1 ;
+    var luongNgay = 100000;
+    var luongThang = luongNgay * soNgaylamviec
+    var resultElement = document.getElementById("result")
+    resultElement.innerHTML = "lương của bạn là : " + luongThang + "đồng";
+   }
--- bai5/index.js
+++ bai5/index.js
@@ -1,8 +1,8 @@
-function Tinhluong() {
-    var soNgaylamviec = document.getElementById("soNgaylamviec").value*1 ;
-    var luongNgay = 100000;
-    var luongThang = luongNgay * soNgaylamviec
-    var resultElement = document.getElementById("result")
-    resultElement.innerHTML = "lương của bạn là : " + luongThang + "đồng";
-
- }
+function tinhChuso () {
+    var chuSo = document.getElementById("chuSo").value*1
+    var hangChuc = Math.floor(chuSo / 10);
+    var hangDonvi = chuSo % 10 ;
+    var tongChusp = hangChuc + hangDonvi;
+    var ketQuaelement = document.getElementById("ketQua")
+    ketQuaelement = "Tổng các chữ số là :" + tongChusp
+}
