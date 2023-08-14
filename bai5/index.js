2e1411c50919c907633690d4da70c263bba59b11
nguyenphanlongvu <vuvt6019@gmail.com>
2023-08-14 09:30:21 +0700

    update file bai5

--- bai5/index.js
+++ bai5/index.js
@@ -2,7 +2,7 @@ function tinhChuso () {
     var chuSo = document.getElementById("chuSo").value*1
     var hangChuc = Math.floor(chuSo / 10);
     var hangDonvi = chuSo % 10 ;
-    var tongChusp = hangChuc + hangDonvi;
+    var tongChuso = hangChuc + hangDonvi;
     var ketQuaelement = document.getElementById("ketQua")
-    ketQuaelement = "Tổng các chữ số là :" + tongChusp
+    ketQuaelement = "Tổng các chữ số là :" + tongChuso
 }
